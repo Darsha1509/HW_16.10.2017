ac71984056db3191a50dbb2953b4fb37976f77de
darya <kokorina.darya1509@gmail.com>
2017-10-25 11:21:34 +0300

    deploy module app engine

--- version-backend/src/main/java/com/example/dasha/backend/VersionApp.java
+++ version-backend/src/main/java/com/example/dasha/backend/VersionApp.java
@@ -1,5 +1,29 @@
 package com.example.dasha.backend;
 
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+@Entity
 public class VersionApp {
 
+    @Id
+    private Long id;
+
+    private String versionCode;
+
+    public VersionApp() {
+    }
+
+    public VersionApp(Long pId, String pVersionCode) {
+        id = pId;
+        versionCode = pVersionCode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getVersionCode() {
+        return versionCode;
+    }
 }
